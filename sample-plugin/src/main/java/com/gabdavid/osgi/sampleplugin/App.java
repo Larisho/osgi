fb378a81dd79d99a09fddb15035a9b08083b9c95
Gabriele Bianchet-David <gbianchetdavid@ebsco.com>
2020-01-05 10:10:20 -0500

    Added comment to sample-plugin

--- sample-plugin/src/main/java/com/gabdavid/osgi/sampleplugin/App.java
+++ sample-plugin/src/main/java/com/gabdavid/osgi/sampleplugin/App.java
@@ -2,6 +2,8 @@ package com.gabdavid.osgi.sampleplugin;
 
 public class App implements Runnable {
 
+  // This is here for the sake of completeness, really. Wouldn't want to tell
+  // Maven that this class has a main method when it doesn't actually have one!
   public static void main(String[] args) {
     new App().run();
   }
